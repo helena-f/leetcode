f5aea97cbd8fa7752b560ec1b516513fa690a0ab
helena-f <43306951+helena-f@users.noreply.github.com>
2025-07-14 23:13:57 -0400

    climb stairs min cost

---
+++ DP/mincost_stairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+        cache = [-1] * n
+        def stair(i):
+            if i >= n:
+                return 0
+
+            if cache[i] != -1:
+                return cache[i]
+
+            cache[i] = cost[i] + min(stair(i+1), stair(i+2))
+            return cache[i]
+        return min(stair(0),stair(1))
