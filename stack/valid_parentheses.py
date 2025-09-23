4c5d7b9e3496e4d53b697161e3f43685fe54d272
helena-f <43306951+helena-f@users.noreply.github.com>
2025-09-23 09:25:22 -0400

    backtracking

--- stack/valid_parentheses.py
+++ stack/valid_parentheses.py
@@ -7,7 +7,24 @@
 # Open brackets are closed in the correct order.
 # Every close bracket has a corresponding open bracket of the same type.
 # Return true if s is a valid string, and false otherwise.
-
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        st = []
+        m = {')': '(', '}':'{',']':'['}
+        
+        for i in s:
+            if i in m.values():
+                st.append(i)
+            
+            elif i in m:
+                if not st or m[i] != st.pop():
+                    return False
+        return (len(st) == 0)
+        
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
